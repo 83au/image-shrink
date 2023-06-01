a5cae524f132924276c35fb9c6e60d3a7ae1f676
Beau Sherman <brsherman88@gmail.com>
2023-06-01 15:03:54 -0700

    Implement menu

--- main.js
+++ main.js
@@ -1,17 +1,107 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
 
-let mainWindow
+// set env
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production'
+const isMac = process.platform === 'darwin'
+
+let mainWindow, aboutWindow
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    backgroundColor: 'aquamarine',
   })
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
   mainWindow.loadFile('./app/index.html')
-  mainWindow.webContents.openDevTools()
+  // if (isDev) mainWindow.webContents.openDevTools()
 }
 
-app.on('ready', createMainWindow)
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: 'aquamarine',
+  })
+
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  aboutWindow.loadFile('./app/about.html')
+  // if (isDev) mainWindow.webContents.openDevTools()
+}
+
+app.on('ready', () => {
+  createMainWindow()
+
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+
+  // Dont' need this because we implemented the same behavior in the menu array
+  // globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+  // globalShortcut.register(isMac ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', () =>
+  //   mainWindow.toggleDevTools()
+  // )
+
+  mainWindow.on('closed', () => (mainWindow = null))
+})
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+]
+
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+})
